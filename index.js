8c62bb3f4633d25c1db0200dac7d826a302ad1fa
Martin Garcia Carral <garciacarralmartin@gmail.com>
2022-09-13 10:04:20 -0300

    archivos

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+let nombre = "martin";
+let saludo = "hola";
+
+let mensaje = `${nombre}  ${saludo}`;
+
+console.log(mensaje);
+console.log("prueba de mensaje sobre la consola");
+console.log("sorete")
+
+for(let index = 0; index = 4; index = index +1){
+console.log(index);
+}
+
